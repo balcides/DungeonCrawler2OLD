111a14e43165ebd67ef20fc971709c5608b07f9d
gabe <balcides@hotmail.com>
2019-04-09 01:53:46 -0700

    91 - Cursor Affordances
    Adding cool cursor icon change

---
+++ Assets/Scripts/CursorAffordance.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorAffordance : MonoBehaviour
+{
+    [SerializeField] Texture2D walkCursor = null;
+    [SerializeField] Texture2D targetCursor = null;
+    [SerializeField] Texture2D unknownCursor = null;
+    [SerializeField] Vector2 cursorHotspot = new Vector2(96, 96);
+
+
+    CameraRaycaster cameraRayCaster;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraRayCaster = GetComponent<CameraRaycaster>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(cameraRayCaster.layerHit){
+            
+            case Layer.Walkable:
+                Cursor.SetCursor(walkCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            case Layer.Enemy:
+                Cursor.SetCursor(targetCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            case Layer.RaycastEndStop:
+                Cursor.SetCursor(unknownCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            default:
+                Debug.LogError("Don't know wahat cursor to show");
+                return;
+        }
+    }
+}
