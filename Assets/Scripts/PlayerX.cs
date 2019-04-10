8fd43ec3ebb01660143f585ed1ec69aa70dd8c69
gabe <balcides@hotmail.com>
2019-04-09 18:09:06 -0700

    105 - Player Health Bar UI

---
+++ Assets/Scripts/PlayerX.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerX : MonoBehaviour
+{
+    [SerializeField] float maxHealthPoints = 100f;
+
+    float currentHealthPoints = 100f;
+
+    //Note: not sure why a get is used here, I'm not well versed but later want to look that up
+    //  answer, helps protect the var and make it read only so it cant be assigned from anywhere
+    public float healthAsPercentage{
+        get{
+            return currentHealthPoints / maxHealthPoints;
+        }
+    }
+}
