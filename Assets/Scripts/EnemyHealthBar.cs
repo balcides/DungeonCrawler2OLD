17a6b9702b1da6b5362e824492bea134d9c07057
gabe <balcides@hotmail.com>
2019-04-09 18:42:28 -0700

    106 - Enemy Floating Health bar

---
+++ Assets/Scripts/EnemyHealthBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealthBar : MonoBehaviour
+{
+    RawImage healthBarRawImage = null;
+    EnemyX enemy = null;
+
+    // Use this for initialization
+    void Start()
+    {
+        enemy = GetComponentInParent<EnemyX>(); // Different to way player's health bar finds player
+        healthBarRawImage = GetComponent<RawImage>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xValue = -(enemy.healthAsPercentage / 2f) - 0.5f;
+        healthBarRawImage.uvRect = new Rect(xValue, 0f, 0.5f, 1f);
+    }
+}
