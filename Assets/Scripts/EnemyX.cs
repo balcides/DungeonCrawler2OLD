c7419059233f671e53a83d2056e28584a3f14836
gabe <balcides@hotmail.com>
2019-04-10 21:23:01 -0700

    122 - Enemy Pathfind Player

--- Assets/Scripts/EnemyX.cs
+++ Assets/Scripts/EnemyX.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 public class EnemyX : MonoBehaviour
 {
     [SerializeField] float maxHealthPoints = 100f;
+    [SerializeField] float attackRadius = 4f;
+    GameObject player = null;
 
     float currentHealthPoints = 100f;
+    AICharacterControl aiCharacterControl = null;
 
     //Note: not sure why a get is used here, I'm not well versed but later want to look that up
     //  answer, helps protect the var and make it read only so it cant be assigned from anywhere
@@ -15,6 +19,25 @@ public class EnemyX : MonoBehaviour
         get
         {
             return currentHealthPoints / maxHealthPoints;
+        }
+    }
+
+    private void Start() {
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        aiCharacterControl = GetComponent<AICharacterControl>();
+    }
+
+    private void Update() {
+        
+        float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+
+        if(distanceToPlayer <= attackRadius){
+
+            aiCharacterControl.SetTarget(player.transform);
+        }else{
+            
+            aiCharacterControl.SetTarget(transform);
         }
     }
 }
